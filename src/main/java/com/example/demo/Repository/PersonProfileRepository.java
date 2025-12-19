374e3ff41f289d65a5ba42eac7290cf4532d7502
bala-amypo <bala@amypo.in>
2025-12-19 06:14:49 +0000

    User commit

---
+++ src/main/java/com/example/demo/Repository/PersonProfileRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.model.PersonProfile;
+import java.util.Optional;
+
+public interface PersonProfileRepository
+        extends JpaRepository<PersonProfile, Long> {
+
+    Optional<PersonProfile> findByEmail(String email);
+    Optional<PersonProfile> findByReferenceId(String referenceId);
+}
